c966e34f337a46801ec44dfd122708738144beb9
bogdan.vakulenko <bogdan.vakulenko@appodeal.com>
2025-09-11 18:53:33 +0400

    tt

--- main/src/grpc_services.rs
+++ main/src/grpc_services.rs
@@ -12,7 +12,21 @@ pub struct RestaurantServiceImp {}
 
 #[tonic::async_trait]
 impl RestaurantService for RestaurantServiceImp {
+    /*
+    #[utoipa::path(
+        get,
+        path = "/pets/{id}",
+        responses(
+            (status = 200, description = "Pet found successfully", body = Pet),
+            (status = NOT_FOUND, description = "Pet was not found")
+        ),
+        params(
+            ("id" = u64, Path, description = "Pet database id to get Pet for"),
+        )
+    )]
+     */
     async fn get_menu(&self, request: Request<MenuRequest>) -> Result<Response<MenuResponse>, Status> {
+
         println!("!!!!!!!{:?}", request);
         Ok(Response::new(MenuResponse { items: vec![MenuItem{ name: Option::from("Pizza".to_string()), price: 10.0}] }))
     }
@@ -31,6 +45,18 @@ pub fn axum_router_wrapper() -> Router {
             .await
             .map(|resp|Json(resp.into_inner()) )
             .map_err(|e: Status| (StatusCode::INTERNAL_SERVER_ERROR, Json(format!("Error: {}. Status code: {}",error.error_message,e.code()))))
+    }
+
+    async fn get_menu_simple(payload: MenuRequest) -> Result<MenuResponse, (StatusCode, String)> {
+
+        let service = RestaurantServiceImp::default();
+
+        let tonic_request: tonic::Request<MenuRequest> = payload.into_request();
+        let error = InternalErrorResponse{error_message: "oops".parse().unwrap() };
+        service.get_menu(tonic_request)
+            .await
+            .map(|resp|resp.into_inner())
+            .map_err(|e: Status| (StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {}. Status code: {}",error.error_message,e.code())))
     }
 
     Router::new().route("/menu", post(get_menu))
