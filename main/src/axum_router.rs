e35ca0fa5f8081da4ca2171ade5e68942aa700d6
bogdan.vakulenko <bogdan.vakulenko@appodeal.com>
2025-09-13 18:06:40 +0400

    tt

--- main/src/axum_router.rs
+++ main/src/axum_router.rs
@@ -1,17 +1,23 @@
+use std::sync::Arc;
+use async_openai::Client;
 use axum::Router;
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_axum::routes;
 use utoipa_swagger_ui::SwaggerUi;
+use crate::ai::{Ai, OpenIA};
 use crate::endpoints;
 
 pub fn axum_router_wrapper() -> Router {
 
-    let (router,  api_doc) = OpenApiRouter::<()>::new()
+    let open_ai = Arc::new(OpenIA::new(Client::new()));
+
+    let (router,  api_doc) = OpenApiRouter::<Arc<OpenIA>>::new()
         .routes(routes!(endpoints::get_menu))
         .split_for_parts();
 
     Router::new()
         .merge(router)
         .merge(SwaggerUi::new("/swagger-ui").url("/openapi.json", api_doc))
+        .with_state(open_ai)
 
 }
--- main/src/endpoints.rs
+++ main/src/endpoints.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
+use std::sync::Arc;
 use async_openai::Client;
+use axum::extract::State;
 use axum::Json;
 use http::StatusCode;
 use crate::ai::OpenIA;
@@ -17,9 +19,7 @@ use crate::transport::transport::{ExplainWordRequest, ExplainWordResponse, TtsRe
          content = ExplainWordRequest,
     )
 )]
-pub async fn get_menu(Json(payload): Json<ExplainWordRequest>) -> Result<Json<ExplainWordResponse>, (StatusCode, Json<String>)> {
-    let open_ai  = OpenIA::new(Client::new());
-
+pub async fn get_menu(State(open_ai): State<Arc<OpenIA>>, Json(payload): Json<ExplainWordRequest>) -> Result<Json<ExplainWordResponse>, (StatusCode, Json<String>)> {
     let parts_of_speeches = HashSet::from(["adjective","noun","verb","phrasal verb","adverb", "pronoun", "preposition", "conjunction", "interjection", "other"]);
     let explained_word = open_ai.explain_word(
         &*payload.word,
@@ -45,13 +45,9 @@ pub async fn get_menu(Json(payload): Json<ExplainWordRequest>) -> Result<Json<Ex
          content = TtsRequest,
     )
 )]
-pub async fn tts(Json(payload): Json<TtsRequest>) -> Result<Json<TtsResponse>, (StatusCode, Json<String>)> {
-    let open_ai  = OpenIA::new(Client::new());
-
-    let resp = open_ai.tts(
-        payload.text,
-        payload.instruction
-    ).await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, Json(e.to_string())))?;
+pub async fn tts(State(open_ai): State<Arc<OpenIA>>, Json(payload): Json<TtsRequest>) -> Result<Json<TtsResponse>, (StatusCode, Json<String>)> {
+    let resp = open_ai.tts( payload.text, payload.instruction ).await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, Json(e.to_string())))?;
 
     Ok(Json(TtsResponse{base64_data:resp}))
 }
